c0e987626b8e3a6c03b5485af9f777ffa504ecde
rafiqpdv <142878467+rafiqpdv@users.noreply.github.com>
2026-02-13 17:02:06 +0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MarzukCalendar from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MarzukCalendar />
+  </React.StrictMode>,
+)
